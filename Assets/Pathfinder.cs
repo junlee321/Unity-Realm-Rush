263b7573362e056b456a2480534594f5e6530134
Junlee321 <curlyjun@gmail.com>
2020-06-05 13:09:44 +0800

    Mark explor blocks in blue

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -5,12 +5,52 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
+    [SerializeField] Waypoint startWayPoint, endWayPoint;
+
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
 
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
+        SetStardAndEndColor();
+        ExploreNeighbours();
+    }
+
+    private void ExploreNeighbours()
+    {
+        //Vector2Int gridPos = startWayPoint.GetGridPos();
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = startWayPoint.GetGridPos() + direction;
+            try
+            {
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            }
+            catch
+            {
+                //do nothing
+            }
+            print("Exploring" + explorationCoordinates);
+
+            //int explorGridX = direction.x + gridPos.x;
+            //int explorGridY = direction.y + gridPos.y;
+            //print("Exploring"+ explorGridX + "," + explorGridY);
+        }
+    }
+
+    private void SetStardAndEndColor()
+    {
+        startWayPoint.SetTopColor(Color.red);
+        endWayPoint.SetTopColor(Color.gray);
     }
 
     private void LoadBlocks()
@@ -27,8 +67,6 @@ public class Pathfinder : MonoBehaviour
             {
                 grid.Add(gridPos, waypoint);
             }
-        }
-        print("Loaded" + grid.Count + "blocks");
-        
+        }   
     }
 }
